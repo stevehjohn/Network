76e44848eb78dabe405faabbcb23bee1fe034ab2
Stevo John <stevehuwjohn@gmail.com>
2025-06-06 18:39:52 +0100

    Adding content

--- src/Networks.Desktop/Infrastructure/TileMapper.cs
+++ src/Networks.Desktop/Infrastructure/TileMapper.cs
@@ -31,5 +31,13 @@ public class TileMapper
         _tiles.Add(new Cell(Piece.Straight, Rotation.OneEighty), contentManager.Load<Texture2D>("vertical"));
         
         _tiles.Add(new Cell(Piece.Straight, Rotation.TwoSeventy), contentManager.Load<Texture2D>("horizontal"));
+        
+        _tiles.Add(new Cell(Piece.Tee, Rotation.Zero), contentManager.Load<Texture2D>("east-south-west"));
+        
+        _tiles.Add(new Cell(Piece.Tee, Rotation.Ninety), contentManager.Load<Texture2D>("north-south-west"));
+        
+        _tiles.Add(new Cell(Piece.Tee, Rotation.OneEighty), contentManager.Load<Texture2D>("north-east-west"));
+        
+        _tiles.Add(new Cell(Piece.Tee, Rotation.TwoSeventy), contentManager.Load<Texture2D>("north-east-south"));
     }
 }
